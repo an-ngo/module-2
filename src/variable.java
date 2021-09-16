9f1a5a8c4cca8f0168c48848589818ec713acf8f
an.ngo <phowflowf135@gmail.com>
2021-09-16 10:54:54 +0700

    m

---
+++ src/variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String[] args) {
+        int i=10;
+        float f = 20.5;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'c';
+        String s = 'Ha Noi';
+        
+    }
+}

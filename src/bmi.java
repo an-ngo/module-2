5d9dc2b56dfe2ce3d7e596a79723535ac9822005
an.ngo <phowflowf135@gmail.com>
2021-09-16 11:46:51 +0700

    m

--- src/bmi.java
+++ src/bmi.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class bmi {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        double weight = scanner.nextDouble();
+        double height = scanner.nextDouble();
+        double bmi = weight/height/height;
+        if(bmi<18.5){
+            System.out.println("UnderWeight");
+        }
+        else if(bmi<25){
+            System.out.println("Normal");
+        }
+        else if(bmi<30){
+            System.out.println("OverWeight");
+        }
+        else System.out.println("Obese");
     }
 }

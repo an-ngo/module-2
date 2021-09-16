a972675eb596ecf91c4cb2f2a82d935685590429
an.ngo <phowflowf135@gmail.com>
2021-09-16 11:53:45 +0700

    m

--- src/Hello.java
+++ src/Hello.java
@@ -3,18 +3,10 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        Scanner bscanner = new Scanner(System.in);
-        int a ;
-        String a1;
-        System.out.println("nhap vao so a sau do la chuoi a1");
-        
-        a=scanner.nextInt();
-        System.out.println(a);
-
-        System.out.println("nhap vao chuoi a1");
-        a1=bscanner.nextLine();
-        System.out.println(a1);
-        scanner.close();
-        bscanner.close();
+        String a;
+        System.out.println("Enter your name:");
+        System.out.println("nhap vao chuoi a");
+        a=scanner.nextLine();
+        System.out.println("Hello: "+a);
     }
 }
